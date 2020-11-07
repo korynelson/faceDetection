34440cdb40965038192a06e700e26ede6f5d5f07
Kory Nelson <kory.nelson99@gmail.com>
2020-11-07 09:19:45 -0800

    added react webcam to webcam module

--- client/src/components/webcam/index.js
+++ client/src/components/webcam/index.js
@@ -1,15 +1,21 @@
 import React from "react";
+import Webcam from "react-webcam";
+//import Webcam from "react-webcam";
 
-function Webcam() {
+
+
+function WebcamModule() {
     return (
       <div className="card">
         <div className="card-body">
           <p className="card-text">
             Webcam Module Loaded
+            <Webcam />
           </p>
         </div>
       </div>
+
     );
   }
   
-  export default Webcam;
+  export default WebcamModule;

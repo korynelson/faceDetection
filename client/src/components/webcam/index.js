69c2dccf7841eaf8ec36c5e03dd9469c322a8602
Kory Nelson <kory.nelson99@gmail.com>
2020-11-07 09:00:34 -0800

    added component for webcam output

---
+++ client/src/components/webcam/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Webcam() {
+    return (
+      <div className="card">
+        <div className="card-body">
+          <p className="card-text">
+            Webcam Module Loaded
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Webcam;

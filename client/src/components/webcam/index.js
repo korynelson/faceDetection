7d83e50a536c3a302e383b137be50dfd8a4e90dc
Kory Nelson <kory.nelson99@gmail.com>
2020-11-07 09:55:53 -0800

    added video constraints and screenshot capture button

--- client/src/components/webcam/index.js
+++ client/src/components/webcam/index.js
@@ -2,17 +2,38 @@ import React from "react";
 import Webcam from "react-webcam";
 //import Webcam from "react-webcam";
 
-
-
 function WebcamModule() {
+    const videoConstraints = {
+        //width: 640,
+        //height: 480,
+        //facingMode: "user"
+      };
+    
+    const webcamRef = React.useRef(null);
+       
+    const capture = React.useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+        },
+          [webcamRef]
+    );
+
     return (
       <div className="card">
         <div className="card-body">
           <p className="card-text">
             Webcam Module Loaded
-            <Webcam />
           </p>
+          <Webcam 
+            audio={false}
+            height={720}
+            width={1280}
+            videoConstraints={videoConstraints}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            />
         </div>
+        <button onClick={capture}>Capture photo</button>
       </div>
 
     );
